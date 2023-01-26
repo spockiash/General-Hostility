5b220a4e36bf29ec9f9e0f572fc6c6a6b6208d3c
spockiash <spocky@post.cz>
2023-01-26 12:16:15 +0100

    test

--- Enemies/Robot.cs
+++ Enemies/Robot.cs
@@ -4,15 +4,16 @@ using System;
 public partial class Robot : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-
+    public TileMap PathGrid { get; set; }
     public override void _Ready()
     {
-        root
+        PathGrid = (TileMap) this.GetParent().GetNode("TileMap/AstarTileMap");
     }
 
     public override void _PhysicsProcess(double delta)
-	{
-
+    {
+        var walkableCells = PathGrid.GetUsedCells(0);
+        var blockedCells = PathGrid.GetUsedCells(1);
 		MoveAndSlide();
 	}
 }
